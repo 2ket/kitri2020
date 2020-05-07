f660691262d94d0892d6137c108c88b384952189
2ket <hello2ket@gmail.com>
2020-05-07 10:51:42 +0900

    상속 예제

---
+++ pro02/src/ch06/inheritance/BExam.java
@@ -0,0 +1,11 @@
+package ch06.inheritance;
+
+public class BExam {
+
+	public static void main(String[] args) {
+		BusinessMan bm=new BusinessMan("홍길동", "키트리", "교육생");
+		bm.info();
+		
+	}
+
+}
---
+++ pro02/src/ch06/inheritance/BMan.java
@@ -0,0 +1,10 @@
+package ch06.inheritance;
+
+public class BMan {
+	protected String name;
+	
+	
+	public void empName() {
+		System.out.println("이름:"+name);
+	}
+}
