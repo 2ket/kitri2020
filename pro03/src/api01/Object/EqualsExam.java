ed7aead67c2de0c9bf6098b815d9fca0f5679a10
2ket <hello2ket@gmail.com>
2020-05-11 15:25:18 +0900

    export-jar로 lib만들고 build path-add lib로 userLib 사용하기 & Open lib import

---
+++ pro03/src/api01/Object/EqualsExam.java
@@ -0,0 +1,15 @@
+package api01.Object;
+
+/**
+ * @Author : 김경은
+ * @Date : 2020. 5. 11.
+ * @Description : 
+ */
+public class EqualsExam {
+
+	public static void main(String[] args) {
+		String a="hi";
+		String b="hi";
+	}
+
+}
---
+++ pro03/src/api01/Object/MyLib.java
@@ -0,0 +1,19 @@
+package api01.Object;
+
+/**
+ * @Author : 김경은
+ * @Date : 2020. 5. 11.
+ * @Description : 
+ */
+
+import ch06.aa.AA;
+
+public class MyLib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AA a=new AA();
+		a.z=100;
+	}
+
+}
