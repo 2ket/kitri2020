4eebc873129ef693f274070f2cf6d6b56c6ff521
2ket <hello2ket@gmail.com>
2020-05-13 11:06:46 +0900

    StringTokenizer

---
+++ pro03/src/api03/String/StringTokenizer03.java
@@ -0,0 +1,41 @@
+package api03.String;
+import java.util.StringTokenizer;
+/**
+ * @Author : 김경은
+ * @Date : 2020. 5. 13.
+ * @Description : 	util package
+ */
+public class StringTokenizer03 {
+
+	public static void main(String[] args) {
+		String strText="홍길동/이영자/조인성";
+		StringTokenizer st=new StringTokenizer(strText, "/");
+		System.out.println("count:"+st.countTokens());
+		
+		while(st.hasMoreTokens()) {
+			String token=st.nextToken();	//토큰을 하나씩 꺼내옴
+			System.out.println(st.countTokens()+"\t"+token);
+		}
+		
+		//결과 안나옴 분리 다 끝남
+		while(st.hasMoreTokens()) {
+			String token=st.nextToken();	
+			System.out.println(st.countTokens()+"\t"+token);
+		}
+		System.out.println();
+		
+		String strValue="홍길동/이영자*조인성";
+		StringTokenizer stoken=new StringTokenizer(strValue, "/");
+		while(stoken.hasMoreTokens()) {
+			String token=stoken.nextToken();
+			System.out.println(token);
+			
+			StringTokenizer ken=new StringTokenizer(token, "*");
+			while(ken.hasMoreTokens()) {
+				System.out.println(ken.nextToken());
+			}
+		}
+		
+	}
+
+}
