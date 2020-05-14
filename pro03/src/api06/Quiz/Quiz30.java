a377382496ec85e76b61354b25ab7795a6d36c8d
2ket <hello2ket@gmail.com>
2020-05-14 16:45:31 +0900

    Date, DecimalFormat Quiz

---
+++ pro03/src/api06/Quiz/Quiz30.java
@@ -0,0 +1,21 @@
+package api06.Quiz;
+
+import java.text.DecimalFormat;
+
+public class Quiz30 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String str="123,456,777.5";
+		DecimalFormat df=new DecimalFormat("#,###.#");
+		Number num=df.parse(str);
+		double su=num.doubleValue();
+		
+		DecimalFormat df1=new DecimalFormat("0");
+		System.out.println("반올림: "+df1.format(su));
+		DecimalFormat df2=new DecimalFormat("#,###");
+		System.out.println("만단위: "+df2.format(su));
+		
+	}
+
+}
